a030729b00cf4cc3d0283a5f036c15298d437dc8
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2021-02-22 03:06:26 -0500

    first commit

---
+++ src/Components/CheckoutPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CheckoutPage(){
+    return(
+        <h1>
+            These are the items
+        </h1>
+    );
+}
+
+export default CheckoutPage;
---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+    
+        return(
+          
+            <h1>
+              This is the Landing page
+            </h1>
+
+        
+        )
+    }
+
+export default Home
